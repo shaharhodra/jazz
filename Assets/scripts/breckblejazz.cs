f8a23f2bafe80b29bb6c50cdc7f5085088778387
shahar hodera <shaharhodera@gmail.com>
2024-05-22 10:31:13 +0300

    new rep

---
+++ Assets/scripts/breckblejazz.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class breckblejazz : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _replacement;
+	[SerializeField] private float _breakForce = 2;
+	[SerializeField] private float _collisionMultiplier = 100;
+	[SerializeField] private bool _broken;
+	[SerializeField] bool hit;
+	private void Awake()
+	{
+		hit = false;
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (_broken) return;
+
+		if (collision.relativeVelocity.magnitude >= _breakForce && hit)
+		{
+			_broken = true;
+			var replacement = Instantiate(_replacement, transform.position, transform.rotation);
+
+			var rbs = replacement.GetComponentsInChildren<Rigidbody>();
+			foreach (var rb in rbs)
+			{
+				rb.AddExplosionForce(collision.relativeVelocity.magnitude * _collisionMultiplier, collision.contacts[0].point, 2);
+			}
+			hit = false;
+			Destroy(gameObject);
+		}
+
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("jazzbulet"))
+		{
+			Debug.Log("jazz");
+
+			hit = true;
+		}
+	}
+}
