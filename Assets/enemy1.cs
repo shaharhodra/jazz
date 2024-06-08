b7c3025b44daea53dde9222cff5c7648d4ae1c26
shahar hodera <shaharhodera@gmail.com>
2024-06-08 18:19:43 +0300

    shahr

--- Assets/enemy1.cs
+++ Assets/enemy1.cs
@@ -6,7 +6,14 @@ using UnityEngine.AI;
 public class enemy1 : MonoBehaviour
 {
     public Transform player;
-    [SerializeField] NavMeshAgent agent;
+	[SerializeField] NavMeshAgent agent;
+    [SerializeField] private float timer = 5;
+    float buletTime;
+    public GameObject enemyBulet;
+   // public NavMeshAgent enemy;
+   // public Transform Player;
+    public Transform spownPoint;
+    public float enemyBuletSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +23,22 @@ public class enemy1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         agent.destination = player.position;
-        
+        ShoteAtPlayer();
+
+    }
+    void ShoteAtPlayer()
+    {
+        buletTime -= Time.deltaTime;
+        if (buletTime > 0) return;
+        {
+            buletTime = timer;
+        }
+        GameObject bulletobj = Instantiate(enemyBulet, spownPoint.transform.position, spownPoint.transform.rotation) as GameObject;
+        Rigidbody buletrig = bulletobj.GetComponent<Rigidbody>();
+        buletrig.AddForce(buletrig.transform.forward * enemyBuletSpeed);
+        Destroy(bulletobj, 1f);
+
     }
 }
